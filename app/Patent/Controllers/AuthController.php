dd0d033084f93d27410f5addb0f1a55dbac46317
winstor <540124900@qq.com>
2019-12-23 16:18:11 +0800

    init

---
+++ app/Patent/Controllers/AuthController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Patent\Controllers;
+
+use Encore\Admin\Controllers\AuthController as BaseAuthController;
+
+class AuthController extends BaseAuthController
+{
+
+}
--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,9 +1,27 @@
 <?php return array (
+  'encore/laravel-admin' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Encore\\Admin\\AdminServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Admin' => 'Encore\\Admin\\Facades\\Admin',
+    ),
+  ),
   'fideloper/proxy' => 
   array (
     'providers' => 
     array (
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel-admin-ext/multitenancy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Encore\\Admin\\Multitenancy\\MultitenancyServiceProvider',
     ),
   ),
   'laravel/tinker' => 
