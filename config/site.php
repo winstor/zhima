25a76142fe5156b0d2901a01019308d105c0abad
winstor <qq@540124900>
2020-01-23 21:26:29 +0800

    delete

--- config/site.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'certificate_status'=>['审查中','授权未交费','已下证'],
-];
--- resources/lang/zh-CN/patent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'domain_name'            => '热门领域',
-    'cat_name'               => '专利分类',
-    'cat_sn'                 => '分类编号',
-    'Sort'                   => '排序',
-];
